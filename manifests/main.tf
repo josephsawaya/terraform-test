5c518879e28ffe3487f709aaa0d8f4a0001e3a16
Joseph Sawaya <jsawaya@redhat.com>
2022-06-27 12:56:05 -0400

    Update main.tf

--- manifests/main.tf
+++ manifests/main.tf
@@ -1,7 +1,55 @@
 provider "kubernetes" {}
 
-resource "kubernetes_namespace" "example" {
+resource "kubernetes_pod" "test" {
   metadata {
-    name = "my-first-namespace"
+    name = "terraform-example"
+  }
+
+  spec {
+    container {
+      image = "nginx:1.21.6"
+      name  = "example"
+
+      env {
+        name  = "environment"
+        value = "test"
+      }
+
+      port {
+        container_port = 80
+      }
+
+      liveness_probe {
+        http_get {
+          path = "/"
+          port = 80
+
+          http_header {
+            name  = "X-Custom-Header"
+            value = "Awesome"
+          }
+        }
+
+        initial_delay_seconds = 3
+        period_seconds        = 3
+      }
+    }
+
+    dns_config {
+      nameservers = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
+      searches    = ["example.com"]
+
+      option {
+        name  = "ndots"
+        value = 1
+      }
+
+      option {
+        name = "use-vc"
+      }
+    }
+
+    dns_policy = "None"
   }
 }
+
