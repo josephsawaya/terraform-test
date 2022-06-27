adc896baff898ae47ed4144b6b5606bd4edb62c5
Joseph Sawaya <jsawaya@redhat.com>
2022-06-27 09:21:33 -0400

    Update main.tf

--- manifests/main.tf
+++ manifests/main.tf
@@ -1,33 +1,7 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = " ~> 2.13.0"
-    }
-  }
-}
+provider "kubernetes" {}
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx1" {
-  image = docker_image.nginx.latest
-  name = "con1"
-  ports {
-    internal = 80
-    external = 8000
-  }
-}
-
-resource "docker_container" "nginx2" {
-  image = docker_image.nginx.latest
-  name = "con2"
-  ports {
-    internal = 80
-    external = 8001
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "my-first-namespace"
   }
 }
